f6ce612d76a5a816ad993132389d87a5c4353660
GERJUNIOR22 <germannisama@gmail.com>
2025-12-01 09:41:06 -0500

    Fix: Resolver conflictos de merge en app.config.ts y cats-http.service.ts

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-<<<<<<< HEAD
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-=======
 import { provideHttpClient, HTTP_INTERCEPTORS, withInterceptorsFromDi } from '@angular/common/http';
->>>>>>> develop
 
 import { routes } from './app.routes';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
@@ -15,11 +10,7 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-<<<<<<< HEAD
-    provideHttpClient(),
-=======
     provideHttpClient(withInterceptorsFromDi()),
->>>>>>> develop
     {
       provide: HTTP_INTERCEPTORS,
       useClass: ErrorInterceptor,
